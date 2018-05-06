8ef9f729e87a4c70885fc9959b54189cd92a259c
Jiro Mikasa <38857700+jiromikasa@users.noreply.github.com>
2018-05-06 16:29:14 +0900

    エラーメッセージとサクセスメッセージ
    flash[:notice]の導入
    エラーメッセージの日本語化

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,11 +2,22 @@ class TweetsController < ApplicationController
 	def index
 		@tweets = Tweet.all.order(created_at: :desc)
 	end
+
+	def new
+		@tweet = Tweet.new
+	end
 	
 	def create
-		@tweet = Tweet.new(user_id: 1, content: params[:content])
-		@tweet.save
-		redirect_to("/tweets")
+		@tweet = Tweet.new(
+			user_id: 1,
+			content: params[:content]
+			)
+		if @tweet.save
+			flash[:notice] = "つぶやきました。"
+			redirect_to("/tweets")
+		else
+			render("tweets/new")
+		end
 	end
 
 end
